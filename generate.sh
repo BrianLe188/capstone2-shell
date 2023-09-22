45bfa07e709decc6d784a1d0f17b0cc59e0658d5
brian.h <anhkun123456@gmail.com>
2023-09-22 19:07:03 +0700

    new: proto

--- generate.sh
+++ generate.sh
@@ -3,3 +3,5 @@ echo "generate proto $1"
 yarn add @grpc/proto-loader
 
 npx proto-loader-gen-types --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=../$1/src/generated/ ./$2/*.proto
+
+cp -R ./$2 ../$1/src/protos
