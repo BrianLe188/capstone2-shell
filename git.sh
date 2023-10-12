2e2a56c6da5f36e81a5f62d27a739f5b646c0b90
brian.h <anhkun123456@gmail.com>
2023-10-12 12:52:16 +0700

    new: gender-rpc

--- git.sh
+++ git.sh
@@ -1,3 +1,12 @@
+if [[ $1 = "--help" ]] 
+then
+  echo '1: type message (-m, -am, ...)'
+  echo '2: type of commit (fix, mod, new, ...)'
+  echo "3: commit's content"
+  echo '4: branch (main, dev, ...)'
+  exit 0
+fi
+
 if [[ $# = 0 ]] || [[ $# < 4 ]]
 then
   exit 1
